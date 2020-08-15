9049a4e00788f793a58fccbd35d039f57c249dcc
sasatake <t06418as@gmail.com>
2020-08-15 16:06:42 +0900

    add test case for joi validation

--- test/http-create-user/index.test.ts
+++ test/http-create-user/index.test.ts
@@ -1,12 +1,18 @@
 import createUser from "#/http-create-user";
 import createContext from "#/test/lib/context";
 
-describe("test create-user http trigger function with valid body.", () => {
+const validUser = {
+  name: "Bill",
+  email: "test@test.com",
+  birthday: "1990-01-01",
+};
+
+describe("when create-user http trigger function with valid body.", () => {
   const request = {
     headers: {
       "content-type": "application/json",
     },
-    body: { name: "Bill" },
+    body: validUser,
   };
   const context = createContext();
   createUser(context, request);
@@ -22,14 +28,18 @@ describe("test create-user http trigger function with valid body.", () => {
   test("should response application/json content-type header.", () => {
     expect(context.res.headers["Content-Type"]).toEqual("applicaion/json");
   });
+
+  test("should set context bind user data.", () => {
+    expect(context.bindings.user).toEqual(validUser);
+  });
 });
 
-describe("test create-user http trigger function with invalid content type.", () => {
+describe("when create-user http trigger function with invalid content type.", () => {
   const request = {
     headers: {
       "content-type": "application/x-www-form-urlencoded",
     },
-    body: { name: "Bill" },
+    body: validUser,
   };
   const context = createContext();
   createUser(context, request);
@@ -39,10 +49,56 @@ describe("test create-user http trigger function with invalid content type.", ()
   });
 
   test("should response invalid message.", () => {
-    expect(context.res.body.message).toEqual("Content Type Header is invalid.");
+    expect(context.res.body.message).toEqual(
+      '"headers.content-type" must be [application/json]'
+    );
   });
 
   test("should response application/json content-type header.", () => {
     expect(context.res.headers["Content-Type"]).toEqual("applicaion/json");
   });
+
+  test("should not set context bind user data.", () => {
+    expect(context.bindings.user).toBeUndefined();
+  });
 });
+
+describe.each([
+  ["", '"body" must be of type object'],
+  [{}, '"body.name" is required'],
+  [{ name: "" }, '"body.name" is not allowed to be empty'],
+  [{ name: "test" }, '"body.email" is required'],
+  [{ name: "test", email: "test" }, '"body.email" must be a valid email'],
+  [
+    { name: "test", email: "test@test.com", birthday: "1990/01/01" },
+    '"body.birthday" must be in iso format',
+  ],
+])(
+  "when create-user http trigger function with invalid request body %#.",
+  (body, expected) => {
+    const request = {
+      headers: {
+        "content-type": "application/json",
+      },
+      body,
+    };
+    const context = createContext();
+    createUser(context, request);
+
+    test("should response 400 http status.", () => {
+      expect(context.res.status).toEqual(400);
+    });
+
+    test("should response invalid message.", () => {
+      expect(context.res.body.message).toEqual(expected);
+    });
+
+    test("should response application/json content-type header.", () => {
+      expect(context.res.headers["Content-Type"]).toEqual("applicaion/json");
+    });
+
+    test("should not set context bind user data.", () => {
+      expect(context.bindings.user).toBeUndefined();
+    });
+  }
+);
